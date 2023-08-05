7e9bbb9cc992ed5ee937652c8f6b603fc8e1acf8
Solaris <ovvo.system@gmail.com>
2023-08-05 12:53:34 -0300

    Update game.js grid variable to contain tile value

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-// The game grid, tiles with a 0 are empty while tiles with a 1 are occupied
+// The game grid, tiles with a 0 are empty
 const grid = [[0,0,0,0],
               [0,0,0,0],
               [0,0,0,0],
@@ -13,17 +13,18 @@ function createTile() {
     let row;
     let col;
     while (true) {
-        // Repeats until an empty tile is found, then marks it as filled
+        // Repeats until an empty tile is found
         row = Math.floor((Math.random()*4));
         col = Math.floor((Math.random()*4));
         if (!grid[row][col]) {
-            grid[row][col] = 1;
             break;
         }
     }
 
-    // Display new tile
-    const tile = document.querySelector(`.grid-tile:nth-child(${row * 4 + col + 1})`);
+    // Updates grid and displays new tile
+    grid[row][col] = 2;
+    const gridPosition = row * 4 + col + 1;
+    const tile = document.querySelector(`.grid-tile:nth-child(${gridPosition})`);
     tile.children[0].textContent = "2";
     styleTile(tile);
 }
